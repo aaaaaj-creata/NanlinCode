fec5ca38d460f90be79576f03265c643d1062163
nanlinw <nanlinw@163.com>
2019-11-25 15:26:41 +0800

    第一次提交

---
+++ Code_C3/1073求偶数数位和.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n,b,c=0 ;
+	scanf("%d",&n) ;
+	while( n != 0 )
+	{
+		b = n % 10 ;
+		if( b % 2 == 0)
+		c = c + b ; 
+		n = n / 10 ;
+	}
+	printf("%d",c) ;
+	
+	return 0;
+}
