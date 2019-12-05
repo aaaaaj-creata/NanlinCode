4246666b6b17e1af7071010f5b7b1d5278831c47
nanlinw <nanlinw@163.com>
2019-12-05 19:03:32 +0800

    4

---
+++ Code_C10/三角形面积.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+ {
+ 	double a,b,c,p,s;
+ 	scanf("%lf %lf %lf",&a,&b,&c);
+ 	p=0.5*(a+b+c);
+ 	s=sqrt(p*(p-a)*(p-b)*(p-c));
+ 	printf("%.3lf",s);
+ 	
+ 	
+ 	
+ 	
+ 	
+    return 0;
+ }
