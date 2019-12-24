12264a7b85ba80756b43e781452e5b43e2b66904
nanlinw <nanlinw@163.com>
2019-12-24 20:20:59 +0800

    1

---
+++ exercise_11_5/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+namespace Nanlin
+{
+	int a = 1;
+	int b = 2;
+}
+
+int main()
+{
+	cout << Nanlin::a << endl;
+	cout << Nanlin::b << endl;
+
+	return 0;
+}
