3f68d045fac4a3b771f709e280160a5f11967ee5
nanlinw <nanlinw@163.com>
2019-12-25 12:50:56 +0800

    1

---
+++ exercise_11_12/源.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int mian()
+{
+	int i;
+		int t;
+	int sum = 0;
+	for (t = i = 1; i <= 10;)
+	{
+		sum += t;
+		++i;
+		if (i % 3 == 0) t = -i;
+		else t = i;
+	}
+	printf("%d", sum);
+	return 0;
+}
