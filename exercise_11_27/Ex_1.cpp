3f4297dd299ef9de5385f99dd24cb1476af1567e
nanlinw <nanlinw@163.com>
2019-12-27 18:55:23 +0800

    1

---
+++ exercise_11_27/Ex_1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int fib(int n)
+{
+	if(n==1)
+	{
+		return 1;
+	}
+	if(n==2)
+	{
+		return 2;
+	}
+	else
+	{
+	return fib(n-1) + fib(n-2);
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",fib(n));
+	return 0;
+}
