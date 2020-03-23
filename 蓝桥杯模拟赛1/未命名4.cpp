15a254041d4fe7ca223543c10e8a9d2c32f3450d
nanlinw <nanlinw@163.com>
2020-03-23 11:21:46 +0800

    4

---
+++ 蓝桥杯模拟赛1/未命名4.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+char str[233];
+int f[4],pos,len;
+inline bool isy(char x) {
+    if (x=='a' || x=='e' || x=='i' || x=='o' || x=='u')
+        return true;
+    else return false;
+}
+int main() {
+    scanf("%s",str),len=strlen(str);
+    while (!isy(str[pos]) && pos<len) f[0]=true,pos++;
+    while (isy(str[pos]) && pos<len) f[1]=true,pos++;
+    while (!isy(str[pos]) && pos<len) f[2]=true,pos++;
+    while (isy(str[pos]) && pos<len) f[3]=true,pos++;   
+    puts(f[0]&&f[1]&&f[2]&&f[3]&&len==pos?"yes":"no");
+    return 0;
+}
