c8f283af140defd893fdd8f513f22bca5e5f72ed
nanlinw <nanlinw@163.com>
2020-04-18 09:31:40 +0800

    3

---
+++ 蓝桥杯校内选拔赛/Ex_3.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void Maop(int list[],int n)
+{
+	
+}
+
+int main()
+{
+	int n=0,k=0;
+	int a=0,sum=0,num=0;
+	int arr[]={0};
+	scanf("%d %d",&n,&k);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d ",&a);
+		arr[i]=a;
+	}
+	Maop(arr,n);
+	num=arr[0];
+	for(i=0;i<n;i++)
+	{
+		
+	}
+}
