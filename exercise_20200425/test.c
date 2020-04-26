9d400c427787613e79c8ff57cc52b1661872fea2
nanlinw <nanlinw@163.com>
2020-04-26 11:53:27 +0800

    1

---
+++ exercise_20200425/test.c
@@ -0,0 +1,37 @@
+//#include<bits/stdc++.h>
+
+//int main()
+//{
+//	int n = 1,i=1,sum=0;
+//	int a = 0, b = 0, c = 0;
+//	scanf_s("%d", &n);
+//	scanf_s("%d %d %d", &a,&b,&c);
+//	while (i <= n)
+//	{
+//		if (i % a != 0 && i % b != 0 && i % c != 0)
+//		{
+//			sum++;
+//		}
+//		i++;
+//	}
+//	printf("%d", sum);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char a[101];
+//	int len, i;
+//	scanf_s("%s", a);
+//	len = strlen(a);
+//	for (i = 0; i < len; i++)
+//	{
+//		a[i] = (a[i] - 97 + 3) % 26 + 97;
+//	}
+//	printf("%s", a);
+//	return 0;
+//}
+
+
