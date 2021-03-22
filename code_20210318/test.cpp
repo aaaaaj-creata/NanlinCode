bc06f638253c4d73d6f9aa1c46bdc5a10e047589
王南蔺 <48408937+NanlinW@users.noreply.github.com>
2021-03-23 00:32:05 +0800

    Add files via upload

---
+++ code_20210318/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+using namespace std;
+//1.将链表转换为二叉树
+//2.判断链表是否带环，入口地址
+bool judge(ListNode* head)
+{
+	ListNode* high = nullptr;
+	ListNode* low = nullptr;
+
+}
+
+int main()
+{
+
+}
